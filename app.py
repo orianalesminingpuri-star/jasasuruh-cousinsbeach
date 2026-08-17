3d45b480224e625a05f429a23e5e4f68e81e3120
orianalesminingpuri-star <orianalesminingpuri@gmail.com>
2026-08-17 20:52:02 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,41 +1,51 @@
 from flask import Flask, render_template
 
-app = Flask(_name_)
+app = Flask(__name__)
 
-SITE_NAME = "Jasasuruh.Cousinsbeach"
-TAGLINE = "Butuh bantuan? Kami yang jalan."
-DESCRIPTION = "Jasa suruh, titip, ambil, dan antar untuk memudahkan aktivitasmu."
-WHATSAPP = "628891006445"
-INSTAGRAM = "https://www.instagram.com/jasasuruh.cousinsbeach?igsh=NjJzZTZ1bmwzcHk0"
-TIKTOK = "https://www.tiktok.com/@sikmapipel5?_r=1&_t=ZS-98wn8fkLGXh"
-AREA = "Cirebon & sekitarnya"
-OPENING_HOURS = "Setiap hari • 08.00–22.00"
-
-SERVICES = [
-    ("🛒", "Belanja & Titip", "Titip makanan, kebutuhan harian, dan barang lainnya."),
-    ("📦", "Ambil & Antar", "Kami ambil paket atau barang lalu mengantarkannya."),
-    ("📄", "Antar Dokumen", "Antar dokumen dan kebutuhan administrasi."),
-    ("✨", "Jasa Lainnya", "Punya kebutuhan khusus? Ceritakan detailnya."),
+drivers = [
+    {
+        "name": "Conrad",
+        "character": "Conrad Fisher",
+        "actor": "Christopher Briney",
+        "status": "online",
+        "area": "Cirebon Kota",
+        "phone": "6289530522457",
+    },
+    {
+        "name": "Jeremiah",
+        "character": "Jeremiah Fisher",
+        "actor": "Gavin Casalegno",
+        "status": "online",
+        "area": "Kabupaten Cirebon",
+        "phone": "6285211003707",
+    },
+    {
+        "name": "Cam",
+        "character": "Cam Cameron",
+        "actor": "David Iacono",
+        "status": "offline",
+        "area": "Kota Bandung",
+        "phone": "6282249152197",
+    },
+    {
+        "name": "Steven",
+        "character": "Steven Conklin",
+        "actor": "Sean Kaufman",
+        "status": "offline",
+        "area": "Kabupaten Bandung",
+        "phone": "6281222729005",
+    }
 ]
 
-DRIVERS = [
-    {"name":"Conrad","photo":"driver1.jpg","area":"Cirebon & sekitarnya","status":"Tersedia","wa":"6289530522457"},
-    {"name":"Jeremiah","photo":"driver2.jpg","area":"Cirebon Kota","status":"Tersedia","wa":"6285211003707"},
-    {"name":"Cam","photo":"driver3.jpg","area":"Kabupaten Cirebon","status":"Offline","wa":"6282249152197"},
-]
 
 @app.route("/")
 def home():
     return render_template(
         "index.html",
-        site_name=SITE_NAME,
-        tagline=TAGLINE,
-        description=DESCRIPTION,
-        whatsapp=WHATSAPP,
-        instagram=INSTAGRAM,
-        tiktok=TIKTOK,
-        area=AREA,
-        opening_hours=OPENING_HOURS,
-        services=SERVICES,
-        drivers=DRIVERS
+        drivers=drivers,
+        admin="628891006445"
     )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
